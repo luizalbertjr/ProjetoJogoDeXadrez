d695e62e0e4a37ab45ee50be1d4c8dfb41455261
Luiz Alberto <luizalbertjr93@gmail.com>
2023-05-14 19:06:39 -0300

    Projeto Criado

---
+++ ProjetoJogoDeXadrez/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProjetoJogoDeXadrez
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
